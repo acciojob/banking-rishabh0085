9591484bd5394f8bb0b6e6b548c8b48a05fa3b94
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-12-26 20:22:30 +0530

    Update SavingsAccount.java

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,28 +1,35 @@
 package com.driver;
 
-public class SavingsAccount extends BankAccount{
-    double rate;
-    double maxWithdrawalLimit;
+public SavingsAccount extends BankAccount {
 
-    public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
-        // minimum balance is 0 by default
+        double rate;
+        double maxWithdrawalLimit;
 
-    }
-    public void withdraw(double amount) throws Exception {
+public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
+        super(name, balance, 0);
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
+        }
+
+public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
-        // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
+        // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds the maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
-    }
+        if (amount > maxWithdrawalLimit) {
+        throw new Exception("Maximum Withdraw Limit Exceed");
+        }
 
-    public double getSimpleInterest(int years){
-        // Return the final amount considering that bank gives simple interest on current amount
+        super.withdraw(amount);
+        }
 
-    }
+public double getSimpleInterest(int years) {
+        // Return the final amount considering that the bank gives simple interest on the current amount
+        return balance * (1 + rate * years);
+        }
 
-    public double getCompoundInterest(int times, int years){
-        // Return the final amount considering that bank gives compound interest on current amount given times per year
-
-    }
-
-}
+public double getCompoundInterest(int times, int years) {
+        // Return the final amount considering that the bank gives compound interest on the current amount given times per year
+        return balance * Math.pow((1 + rate / times), times * years);
+        }
+        }
